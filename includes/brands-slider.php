ef678c375ba776673511e55dd19a129d4192e50c
Git-sahilpatel <sp1030564@gmail.com>
2025-06-12 10:27:13 +0530

    Initial commit

---
+++ includes/brands-slider.php
@@ -0,0 +1,80 @@
+<div id="brands-carousel" class="logo-slider wow fadeInUp">
+		<h3 class="section-title">Brands</h3>
+		<div class="logo-slider-inner">	
+			<div id="brand-slider" class="owl-carousel brand-slider custom-carousel owl-theme">
+
+				<div class="item">
+					<a  class="image">
+					<img data-echo="" src="" alt="">RAYMOND
+					</a>	
+				</div><!--/.item-->
+
+				<div class="item">
+					<a  class="image">
+					<img data-echo="" src="" alt="">ZARA
+					</a>	
+				</div><!--/.item-->
+
+				<div class="item">
+					<a  class="image">
+					<img data-echo="" src="" alt="">ALLEN SOLLY
+					</a>	
+				</div>
+
+				<div class="item">
+					<a  class="image">
+					<img data-echo="" src="" alt="">LEOTUDE
+					</a>	
+				</div>
+
+		
+				<div class="item">
+					<a  class="image">
+					<img data-echo="" src="" alt="">U.S. POLO
+					</a>	
+				</div>
+             
+				<div class="item">
+					<a  class="image">
+					<img data-echo="" src="" alt="">H&M
+					</a>	
+				</div>
+            
+				<div class="item">
+					<a  class="image">
+					<img data-echo="" src="" alt="">ADIDAS
+					</a>	
+				</div>
+
+				<div class="item">
+					<a  class="image">
+					<img data-echo="" src="" alt="">NIKE
+					</a>	
+				</div>
+
+				<div class="item">
+					<a  class="image">
+					<img data-echo="" src="" alt="">PRADA
+					</a>	
+				</div>
+
+				<div class="item">
+					<a  class="image">
+					<img data-echo="" src="" alt="">CALVIN KLEIN
+					</a>	
+				</div>
+
+				<div class="item">
+					<a  class="image">
+					<img data-echo="" src="" alt="">GUCCI
+					</a>	
+				</div>
+
+
+
+
+
+		    </div><!-- /.owl-carousel #logo-slider -->
+		</div><!-- /.logo-slider-inner -->
+	
+</div><!-- /.logo-slider -->
